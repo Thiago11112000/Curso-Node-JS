ebcd62553d97b46fc35b8951687dff27a4772310
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-05-14 13:57:23 -0300

    Primeira rota

--- 001 - Introducao-nodejs/index.js
+++ 001 - Introducao-nodejs/index.js
@@ -1,1 +1,15 @@
-console.log(3000)
+import http from "node:http";  // comando typescript
+
+const  server = http.createServer((req,res) =>{
+    res.statusCode = 404;
+    return res.end(
+        JSON.stringify({
+         status: 404,
+         body: "404 (NOT  found)",
+        })
+    )
+});
+    
+    server.listen(3000, () =>{
+        console.log("Deu bom")
+    });
