61e30ece33442d348a8287131357c6847809e6d6
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-05-14 18:11:03 -0300

    Multiplas rotas

--- 001 - Introducao-nodejs/index.js
+++ 001 - Introducao-nodejs/index.js
@@ -1,6 +1,30 @@
 import http from "node:http";  // comando typescript
 
 const  server = http.createServer((req,res) =>{
+const {url, method} = req; 
+
+
+   if(url === '/' && method === "GET"){
+    return res.end(JSON.stringify({
+    status: 200,
+    body: "Bem vindo(a) a home",
+    })
+);
+   }
+if(url === '/contato' && method === "GET"){
+    return res.end(JSON.stringify({
+    status: 200,
+    body: "Entre em contato",
+    })
+);
+}
+   if(url === '/' && method === "POST"){
+    return res.end(JSON.stringify({
+    status: 200,
+    body: "Bem vindo(a) a home - COM O METHOD POST",
+    })
+);
+   }
     res.statusCode = 404;
     return res.end(
         JSON.stringify({
